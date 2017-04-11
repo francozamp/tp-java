d7bc49b6022d468c65e018d48d0f4c6e4373f073
valinotti4 <lupi.valinotti@gmail.com>
2017-04-10 23:26:39 -0300

    Registrar usuario completo

--- proyecto_libros/src/Entidades/Usuario.java
+++ proyecto_libros/src/Entidades/Usuario.java
@@ -4,31 +4,63 @@ import Datos.DatosEstado;
 import Datos.DatosTipoUsuario;
 
 public class Usuario {
-	
-	public String email;
-	public String nombre;
-	public String apellido;
-	public String telefono;
-	public String direccion;
-	public String password;
-	public Estado estado;
-	public TipoUsuario tipoUsuario;
-	
-	public Usuario(String email,String nombre, String apellido, String telefono,String direccion, String pass, int idEstado, int idTipoUsu){
-		
-		this.email=email;
-		this.nombre=nombre;
-		this.apellido=apellido;
-		this.telefono=telefono;
-		this.direccion=direccion;
-		this.password=pass;
-		
-		DatosEstado de=new DatosEstado();
-		this.estado=de.getEstado(idEstado);
-		
-		DatosTipoUsuario dtu=new DatosTipoUsuario();
-		this.tipoUsuario=dtu.getTipoUsuario(idTipoUsu);
-		
-	}
+
+    private String email;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String direccion;
+    private String password;
+    private Estado estado;
+    private TipoUsuario tipoUsuario;
+
+    public Usuario(String email, String nombre, String apellido, String telefono, String direccion, String pass, int idEstado, int idTipoUsu) {
+
+        this.email = email;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.password = pass;
+
+        DatosEstado de = new DatosEstado();
+        this.estado = de.getEstado(idEstado);
+
+        DatosTipoUsuario dtu = new DatosTipoUsuario();
+        this.tipoUsuario = dtu.getTipoUsuario(idTipoUsu);
+
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getApellido() {
+        return this.apellido;
+    }
+
+    public String getTelefono() {
+        return this.telefono;
+    }
+
+    public String getDireccion() {
+        return this.direccion;
+    }
+
+    public String getContrasena() {
+        return this.password;
+    }
+
+    public int getEstado() {
+        return this.estado.getID();
+    }
+
+    public int getTipousu() {
+        return this.tipoUsuario.getID();
+    }
 
 }
--- proyecto_libros/src/Negocio/NegocioUsuario.java
+++ proyecto_libros/src/Negocio/NegocioUsuario.java
@@ -4,17 +4,24 @@ import Datos.DatosUsuario;
 import Entidades.Usuario;
 
 public class NegocioUsuario {
-	
-	public Usuario hacerLogin(String email, String pass){
-		
-		Usuario usu = null;
-		
-		DatosUsuario dUsuario=new DatosUsuario();
-		usu=dUsuario.hacerLogin(email, pass);
-		
-		return usu;
-		
-		
-	}
+
+    public Usuario hacerLogin(String email, String pass) {
+
+        Usuario usu = null;
+
+        DatosUsuario dUsuario = new DatosUsuario();
+        usu = dUsuario.hacerLogin(email, pass);
+
+        return usu;
+
+    }
+
+    public Usuario guardarUsuario(Usuario usuario) {
+
+        DatosUsuario datosUsuario = new DatosUsuario();
+        usuario = datosUsuario.guardarUsuario(usuario);
+
+        return usuario;
+    }
 
 }
